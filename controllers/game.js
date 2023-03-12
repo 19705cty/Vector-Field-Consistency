641522a2e48099257d946a50b58b8a53dda40d87
Andy Jou <1997cty@gmail.com>
2023-03-12 00:24:16 -0800

    added update object api and class method

--- controllers/game.js
+++ controllers/game.js
@@ -2,14 +2,14 @@ import gameData from "../models/gameData.js"
 import { GameObject, Client, Server} from "../models/VFC.js";
 
 async function logGameInfo(req, res) {
-  console.log(gameData)
+  // console.log(gameData)
+  // console.log(gameData.gameObjects[3])
   return res.status(200).json({
     msg: "only show on the server side."
   });
 }
 
 async function startNewGame(req, res) {
-  // gameData.setPositionLimit(69)
   return res.status(200).json({
     msg: "only show on the server side."
   });
